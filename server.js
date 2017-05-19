96ec5d13618f25f8f5f39734bdf1e6c61e624c64
Benilyn <benilyn.gonzales@gmail.com>
2017-05-18 22:52:38 -0700

    changed runServer and closeServer function to include mongoose

--- server.js
+++ server.js
@@ -3,13 +3,17 @@
 const express    = require('express');
 const router     = express.Router();
 const morgan     = require('morgan');
+const mongoose	 = require('mongoose');
+const app 		 = express();
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-const app        = express();
-
 const blogPostsRouter = require('./blogPostsRouter');
+
+const {PORT, DATABASE_URL} = require('./config');
+const {Blog} = require('./models');
+
 
 
 app.use(morgan('common'));
@@ -22,31 +26,37 @@ app.get('/', (req, res) => {
 
 let server;
 
-function runServer() {
-	const port = process.env.PORT || 8080;
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
 	return new Promise((resolve, reject) => {
-		server = app.listen(port, () => {
-			console.log('Your app is listening on the ${port}');
-			resolve(server);
-		}) //server
-		.on('error', err => {
-			reject(err);
-		}); //.on(error)
+		mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on the port ${port}`);
+				resolve();
+			}) //server = app.listen
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			}); //.on(error)
+		}); //mongoose.connect
 	}); //return new Promise
-} //function runServer
+} //runServer function
 
 function closeServer() {
-	return new Promise((resolve, reject) => {
-		console.log('Closing server');
-		server.close(err => {
-			if (err) {
-				reject(err);
-				return;
-			} //if(err)
-			resolve();
-		}); //server.close(err)
-	}); //return new Promise
-} //function closeServer
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			}); //server.close
+		}); //return new Promise
+	}); //return mongoose.disconnect
+} //closeServer function 
 
 if (require.main === module) {
   runServer().catch(err => console.error(err));
