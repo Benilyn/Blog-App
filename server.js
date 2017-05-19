d1a7797034516bfb4b51a994c77f9a5ee61ad7a7
Benilyn <benilyn.gonzales@gmail.com>
2017-05-19 11:39:39 -0700

    finish PUT request

--- server.js
+++ server.js
@@ -65,16 +65,33 @@ app.post('/posts', (req, res) => {
 		}); //.catch
 }); //app.post
 
-app.get('/posts/:id', (req, res) => {
+app.put('/posts/:id', (req, res) => {
+	if (!(req.body.id && req.params.id === req.body.id)) {
+		const message = (
+			`Request path id (${req.params.id}) and request body id` +
+			`(${req.body.id}) must match`);
+		console.error(message);
+		res.status(400).json({message: message});
+	}
+
+	const toUpdate = {};
+	const updateableFields = ['title', 'content', 'author'];
+
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			toUpdate[field] = req.body[field];
+		}
+	}); //updateableFields.forEach
+
 	Post
 		.findById(req.params.id)
 		.exec()
 		.then(post => res.json(post))
 		.catch(err => {
 			console.error(err);
-				res.status(500).json({message: 'Internal server error'})
+				res.status(500).json({message: 'Internal server error'});
 		}); //.catch
-}); //app.get
+}); //app.put
 
 
 
